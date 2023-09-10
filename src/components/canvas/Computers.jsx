0bec32c1f236a92a64358c51933ad7c5299d2af9
unknown <9012257+jeiku@users.noreply.github.com>
2023-09-09 23:25:42 -0400

    adding new model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,22 +8,43 @@ const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
+    // <mesh>
+    //   <hemisphereLight intensity={1} groundColor='black' />
+    //   <spotLight
+    //     position={[-20, 50, 10]}
+    //     angle={0.12}
+    //     penumbra={1}
+    //     intensity={1}
+    //     castShadow
+    //     shadow-mapSize={1024}
+    //   />
+    //   <pointLight intensity={1} />
+    //   <primitive
+    //     object={computer.scene}
+    //     scale={isMobile ? 0.7 : 0.75}
+    //     position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+    //     rotation={[-0.01, -0.2, -0.1]}
+    //   />
+    // </mesh>
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={1} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
-        castShadow
-        shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight
+        intensity={1}
+        position={[10, 0, 0]}
+        scale={7}
+        rotation={[-0.01, -1.5, -0.2]}
+      />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.7 : 2.25}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        rotation={[-0.01, -1.5, -0.2]}
       />
     </mesh>
   );
@@ -61,9 +82,11 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
+      {/* REact- loader to be there while model is loading */}
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
+          // Allows us to rotate it only around a specific angle
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
